2e533d593bc63c91f9c9f93029efcff925e88de5
niiicolai <niiicolai@gmail.com>
2024-09-25 21:15:42 +0200

    fix all endpoints

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
-venv\Scripts\activate
+
+# Development
+
+## Virtual environment
 Create a virtual environment and activate it:
-   
-   python -m venv venv
-   source venv/bin/activate  # On Windows use `venv\Scripts\activate`
-   
+```
+python -m venv venv
+```
 
+Activate:
+```
+source venv/bin/activate  # UNIX
+venv\Scripts\activate # Windows
+```
+
+Verify it is active:
+```
+which python
+```
+Should return a string pointing to the Python binaries stored within `path-to-project/project/venv/bin/python`.
+
+## Install dependencies
+*Note: Ensure you have created and activated the virtual environment first*
+```
 pip install -r requirements.txt
+```
 
-to start project:
-    fastapi dev main.py
+## Start Server
+```
+   fastapi dev main.py
+```
+
+## Frontend
+1. Go to https://github.com/niiicolai/js_fake_info_frontend and download the repository.
+2. Download the 'Express' plugin or another web-server plugin for visual code.
+3. Start the server by pressing Ctrl+Shift+P, type 'express' and select 'Host: current workspace'
+
---
+++ src/data/fake_person.py
@@ -0,0 +1,108 @@
+# Inspired by https://github.com/arturomorarioja/fake_info/blob/main/src/FakeInfo.php
+import json
+import random
+
+# Load the data from the json file
+with open('./src/data/person-names.json') as f:
+    data = json.load(f)
+
+class FakePerson:
+    """
+        FakePerson class is used to generate fake person data 
+        for testing purposes.
+    """
+
+    def __init__(self):
+        """
+            The constructor automatically generates
+            the data when an instance of the class is created.
+        """
+        randomPerson = random.choice(data['persons'])
+        self.firstName = randomPerson['firstName']
+        self.lastName = randomPerson['lastName']
+        self.gender = randomPerson['gender']
+        self.set_birth_date()
+        self.set_cpr()
+        self.set_address()
+        self.set_phone_number()
+        self.set_address()
+        self.set_phone_number()
+
+    def set_birth_date(self):
+        """
+            Set the birth date of the person.
+        """
+        # Get a random year between 1900 and 2021
+        year = random.randint(1900, 2021)
+        # Get a random month between 1 and 12
+        month = random.randint(1, 12)
+        # Get a random day depending on the month
+        if month in [1, 3, 5, 7, 8, 10, 12]:
+            day = random.randint(1, 31)
+        elif month in [4, 6, 9, 11]:
+            day = random.randint(1, 30)
+        else:
+            # Leap years are not taken into account
+            # so as not to overcomplicate the code
+            day = random.randint(1, 28)
+        if len(str(month)) == 1:
+            month = f"0{month}"
+        if len(str(day)) == 1:
+            day = f"0{day}"
+        self.birthDate = f"{year}-{month}-{day}"
+
+    def set_cpr(self):
+        """
+            Set the cpr of the person.
+        """
+        if not hasattr(self, 'birthDate'):
+            raise Exception("Birth date must be set before generating CPR")
+        if not hasattr(self, 'gender'):
+            raise Exception("Gender must be set before generating CPR")
+
+        # last digit must be even for females and odd for males
+        last_digit = random.randint(0, 9)
+        if self.gender == "male" and last_digit % 2 == 0:
+            last_digit = (last_digit + 1) % 10
+        elif self.gender == "female" and last_digit % 2 != 0:
+            last_digit = (last_digit + 1) % 10
+
+        # Get day as the 8th and 9th character of the birth date
+        day = self.birthDate[8:10]
+        # Get month as the 5th and 6th character of the birth date
+        month = self.birthDate[5:7]
+        # Get year as the 2nd and 3rd character of the birth date
+        year = self.birthDate[2:4]
+        # Three complete random digits
+        random_digits = random.randint(100, 999)
+        # Format: DDMMYY-XXXX
+        self.CPR = f"{day}{month}{year}{random_digits}{last_digit}"
+
+    def set_address(self):
+        """
+            Set the address of the person.
+        """
+        # the address variable must be named 'address'
+        # to match how the frontend expects the data
+        # https://github.com/arturomorarioja/js_fake_info_frontend/blob/main/js/script.js#L70-L76
+        # Also, if you need help making the method generating the address,
+        # look at: https://github.com/arturomorarioja/fake_info/blob/main/src/FakeInfo.php#L108-L162
+        self.address = {
+            "street": "Random street",
+            "number": 1,
+            "floor": 1,
+            "door": "th",
+            "postal_code": "2222",
+            "town_name": "Random town name",
+        }
+
+    def set_phone_number(self):
+        """
+            Set the phone number of the person.
+        """
+        # the phone number variable must be named 'phoneNumber'
+        # to match how the frontend expects the data
+        # https://github.com/arturomorarioja/js_fake_info_frontend/blob/main/js/script.js#L79-L82
+        # Also, if you need help making the method generating the phone number,
+        # look at: https://github.com/arturomorarioja/fake_info/blob/main/src/FakeInfo.php#L196-L205
+        self.phoneNumber = "4512345678"
