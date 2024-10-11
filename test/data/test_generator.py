d12404f452d284162c02efd3396a4ef50fdd67d6
niiicolai <niiicolai@gmail.com>
2024-10-11 19:59:44 +0200

    update unit test

--- test/data/test_generator.py
+++ test/data/test_generator.py
@@ -2,6 +2,125 @@ import pytest
 import re
 from src.data.generator import Generator
 
+
+@pytest.mark.parametrize("personList", [
+    [{ "firstName": "" }],
+    [{ "firstName": "Anne Lise " }],
+    [{ "firstName": "Anne..Lise" }],
+    [{ "firstName": "Ann@" }],
+    [{ "firstName": "324543" }],
+    [{ "firstName": "anne" }],
+    [{ "": "Anne" }],
+    [{ "wrongKey": "Anne" }],
+])
+def test_generate_first_name_with_invalid_values(personList):
+    with pytest.raises(ValueError):
+        Generator.generate_first_name(personList), f"First name {personList[0].get('firstName')} is not valid"
+
+
+@pytest.mark.parametrize("personList", [
+    [{ "firstName": "Annemette P." }],
+    [{ "firstName": "Anne-Lise N." }],
+    [{ "firstName": "Børge B." }],
+])
+def test_generate_first_name_returns_valid_name(personList):
+    expected = personList[0].get("firstName")
+    assert Generator.generate_first_name(personList) == expected, f"First name {expected} is not valid" 
+    
+
+@pytest.mark.parametrize("personList", [
+    [{ "lastName": "" }],
+    [{ "lastName": "Anne Lise " }],
+    [{ "lastName": "Anne..Lise" }],
+    [{ "lastName": "Ann@" }],
+    [{ "lastName": "324543" }],
+    [{ "lastName": "anne" }],
+    [{ "": "Anne" }],
+    [{ "wrongKey": "Anne" }],
+])
+def test_generate_last_name_with_invalid_values(personList):  
+    with pytest.raises(ValueError):
+        Generator.generate_last_name(personList), f"Last name {personList[0].get('lastName')} is not valid"
+
+
+@pytest.mark.parametrize("personList", [
+    [{ "lastName": "Simonsen" }],
+    [{ "lastName": "Christoffersen" }],
+    [{ "lastName": "Kjær" }],
+])
+def test_generate_last_name_returns_valid_name(personList):
+    expected = personList[0].get("lastName")
+    assert Generator.generate_last_name(personList) == expected, f"Last name {expected} is not valid" 
+    
+
+@pytest.mark.parametrize("month_no, expected", [
+    (1, (1, 31)), (2, (1, 28)),
+    (3, (1, 31)), (4, (1, 30)),
+    (5, (1, 31)), (6, (1, 30)),
+    (7, (1, 31)), (8, (1, 31)),
+    (9, (1, 30)), (10, (1, 31)),
+    (11, (1, 30)), (12, (1, 31))
+])
+def test_get_day_range_for_month_returns_valid_range(month_no, expected):
+    assert Generator.get_day_range_for_month(month_no) == expected, f"Month {month_no} should have {expected[1]} days"
+    
+    
+@pytest.mark.parametrize("date_ranges, expected", [
+    ({"startY": 1900, "endY": 1900, "startM": 1, "endM": 1}, "1900-01-01"),
+    ({"startY": 1950, "endY": 1950, "startM": 1, "endM": 1}, "1950-01-01"),
+    ({"startY": 2000, "endY": 2000, "startM": 1, "endM": 1}, "2000-01-01"),
+])
+def test_generate_birth_date_returns_correct_format(date_ranges, expected):
+    birthDate = Generator.generate_birth_date(
+        date_ranges["startY"], 
+        date_ranges["endY"], 
+        date_ranges["startM"], 
+        date_ranges["endM"], 
+        {
+            "MONTHS_WITH_31_DAYS": [1, 3, 5, 7, 8, 10, 12],
+            "MONTHS_WITH_30_DAYS": [4, 6, 9, 11],
+            "MONTH_31_RANGE": (1, 1),
+            "MONTH_30_RANGE": (1, 1),
+            "MONTH_28_RANGE": (1, 1)
+        }
+    )
+    assert birthDate == expected, f"Birthday {birthDate} is not in the correct format (YYYY-MM-DD)"
+    
+    
+@pytest.mark.parametrize("date_ranges", [
+    ({"startY": 1901, "endY": 1900, "startM": 1, "endM": 1}),
+    ({"startY": 1900, "endY": 1900, "startM": 2, "endM": 1}),
+    ({"startY": 0,    "endY": 2000, "startM": 1, "endM": 1}),
+    ({"startY": 1900, "endY": 0,    "startM": 1, "endM": 1}),
+    ({"startY": 1900, "endY": 2000, "startM": 0, "endM": 1}),
+    ({"startY": 1900, "endY": 2000, "startM": 1, "endM": 0}),
+])
+def test_generate_birth_date_with_invalid_ranges(date_ranges):
+    with pytest.raises(ValueError):
+        assert Generator.generate_birth_date(
+            date_ranges["startY"], 
+            date_ranges["endY"], 
+            date_ranges["startM"], 
+            date_ranges["endM"], 
+        ), f"Invalid date ranges {date_ranges} should not be allowed"
+        
+        
+@pytest.mark.parametrize("date_ranges", [
+    ({"startY": None, "endY": 2000, "startM": 1, "endM": 0}),
+    ({"startY": 1900, "endY": None, "startM": 1, "endM": 0}),
+    ({"startY": 1900, "endY": 2000, "startM": None, "endM": 0}),
+    ({"startY": 1900, "endY": 2000, "startM": 1, "endM": None}),
+])
+def test_generate_birth_date_with_invalid_types(date_ranges):
+    with pytest.raises(TypeError):
+        assert Generator.generate_birth_date(
+            date_ranges["startY"], 
+            date_ranges["endY"], 
+            date_ranges["startM"], 
+            date_ranges["endM"], 
+        ), f"Date ranges {date_ranges} must be integers"
+    
+
 def test_phone_number_is_correct_format():
     phone_number = Generator.generate_phone_number()
 
@@ -12,81 +131,6 @@ def test_phone_number_is_correct_format():
     assert length == 8, f"Phone number {phone_number} does not have exactly 8 digits"
     assert isDigit, f"Phone number {phone_number} contains non-numeric characters"
     assert hasPrefix, f"Phone number {phone_number} has an invalid prefix"
-
-
-@pytest.mark.parametrize("month_no, expected", [
-    (1, (1, 31)), 
-    (2, (1, 28)),
-    (3, (1, 31)), 
-    (4, (1, 30)),
-    (5, (1, 31)), 
-    (6, (1, 30)),
-    (7, (1, 31)), 
-    (8, (1, 31)),
-    (9, (1, 30)), 
-    (10, (1, 31)),
-    (11, (1, 30)), 
-    (12, (1, 31))
-])
-def test_get_day_range_for_month_returns_valid_range(month_no, expected):
-    assert Generator.get_day_range_for_month(month_no) == expected, f"Month {month_no} should have {expected[1]} days"
-
-
-@pytest.mark.parametrize("date_ranges, expected", [
-    ({"startY": 1900, "endY": 2021, "startM": 1, "endM": 12}, r"^\d{4}-\d{2}-\d{2}$"),
-    ({"startY": 1901, "endY": 2020, "startM": 2, "endM": 11}, r"^\d{4}-\d{2}-\d{2}$"),
-])
-def test_birthday_is_correct_format(date_ranges, expected):
-    birthday = Generator.generate_birth_date(
-        date_ranges["startY"], 
-        date_ranges["endY"], 
-        date_ranges["startM"], 
-        date_ranges["endM"]
-    )
-    assert re.match(expected, birthday), f"Birthday {birthday} is not in the correct format (YYYY-MM-DD)"
-
-
-@pytest.mark.parametrize("date_ranges", [
-    ({"startY": 1900, "endY": 2021, "startM": 1, "endM": 12}), 
-    ({"startY": 1901, "endY": 2020, "startM": 2, "endM": 11})
-])
-def test_birthday_is_within_range(date_ranges):
-    birthday = Generator.generate_birth_date(
-        date_ranges["startY"], 
-        date_ranges["endY"], 
-        date_ranges["startM"], 
-        date_ranges["endM"]
-    )
-    
-    generated_year = int(birthday[:4])
-    generated_month = int(birthday[5:7])
-    generated_day = int(birthday[8:10])
-    
-    assert generated_year >= date_ranges["startY"] and generated_year <= date_ranges["endY"], f"The birthday's year ({birthday}) is outside the range {date_ranges['startY']}-{date_ranges['endY']}"
-    assert generated_month >= date_ranges["startM"] and generated_month <= date_ranges["endM"], f"The birthday's month ({birthday}) is outside the range {date_ranges['startM']}-{date_ranges['endM']}"
-    assert generated_day >= 1 and generated_day <= 31, f"The birthday's day ({birthday}) is outside the range 1-31"
-
-"""
-@pytest.mark.parametrize("name", [
-    "",
-    "Anne Lise ",
-    "Anne..Lise",
-    "Ann@",
-    "324543",
-    "anne",
-])
-def test_validate_last_name_invalid(name):
-    assert FakePerson.validate_last_name(name) == False
-
-@pytest.mark.parametrize("name", [
-    "Simonsen",
-    "Christoffersen",
-    "Kjær",
-])
-def test_validate_last_name_valid(name):
-    assert FakePerson.validate_last_name(name) == True
-"""
-
 
 def test_street_is_correct_format():
      # Arrange
